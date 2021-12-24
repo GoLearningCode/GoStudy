aa15dfc3005d4c9b8e49fd664c63e97c9db093d0
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-12-23 21:54:20 -0300

    updating server

--- server/server.go
+++ server/server.go
@@ -6,11 +6,16 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Hi %s", r.URL.Path[1:])
+func root(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi %s", r.URL.Path[1:])
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "foobar")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/bar", bar)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
